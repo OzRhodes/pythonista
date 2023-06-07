951fc1906faa54fe80cfb70f33a334efef68d4b6
oz rhodes <oz@ozrhodes.com>
2023-06-07 21:21:38 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,65 +1,26 @@
-#app.py the ORFLASK version of a microblog
-''' A Flask app which showcases a microblog'''
+from flask import Flask, render_template
 
-from flask import Flask, url_for, render_template
+# Create our Flask Instance
+
 app = Flask(__name__)
 
+# Create URL Routeshello
 
-
-posts = [
-    { 
-     'post_number' : 1,
-     'post_title' : 'Post1',
-     'post_date' : 'dtg1',
-     'post_content': 'This is content 1',
-     'post_tag' : 'python'
-        
-    },
-    
-    {
-          'post_number' : 2,
-     'post_title' : 'Post2',
-     'post_date' : 'dtg2',
-     'post_content': 'This is content 2',
-     'post_tag' : 'python'   
-    },
-    
-    {
-    'post_number' : 3,
-    'post_title' : 'Post3',
-    'post_date' : 'dtg3',
-    'post_content': 'This is content 3',
-    'post_tag' : 'python'
-    },
-]
-
-
-
-
-
-
-@app.route('/')
+@app.route("/")
 def index():
-    title = 'Oz Rhodes'
-    
-    return render_template('index.html')
+# return "<h1>Hello, World!</h1>"
+     return render_template("index.html")
 
-@app.route('/blog')
-def blog():
-    title = 'OR - Blog'
-    return render_template('blog.html',title = title, posts = posts)
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    title = 'OR - About'
-    return render_template('about.html' ,title = title)
+    return "<p>About!</p>"
 
-@app.route('/contact')
-def contact():
-    title = 'OR - Contact'
-    return render_template('contact.html' ,title = title)
+@app.errorhandler(404)
+def page_not_found(e):
+     return render_template("404.html"), 404
 
-@app.route('/category')
-def category():
-    title = 'OR - Category'
-    return render_template('category.html',title = title)
+
+@app.errorhandler(500)
+def server_error(e):
+     return render_template("server_error.html"), 500
