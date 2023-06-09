826e7437e050964555d2757fe797d91edd626aa1
oz rhodes <oz@ozrhodes.com>
2023-06-09 11:59:57 +0100

    added flask-wtf without CSRF

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired
 
 # Create our Flask Instance
 
 app = Flask(__name__)
 app.config['TESTING'] = True
+app.config['SECRET_KEY'] = "super secret key to be added in future in a more secure way"
 
-# Create URL Routeshello
+# Create URL Routes
 
 @app.route("/")
 def index():
@@ -17,6 +21,17 @@ def index():
 def about():
      return render_template("about.html")
 
+@app.route("/login", methods=['GET', 'POST'] )
+def login():
+     email = None
+     form = LoginForm()
+     #validate
+     if form.validate_on_submit():
+          email = form.email.data
+          form.email.data = ''
+     return render_template("login.html", email = email, form = form)
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,3 +41,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
      return render_template("server_error.html"), 500
+
+# Forms
+# create a form class
+class LoginForm(FlaskForm):
+     email = EmailField("Email", validators=[DataRequired()])
+     submit = SubmitField("Submit")
+
+
+
